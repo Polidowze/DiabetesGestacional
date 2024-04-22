0c23e866415f73683b7f75183f56e62015fc2eef
polidow <162759946+polidow@users.noreply.github.com>
2024-04-22 19:06:06 -0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -2,51 +2,31 @@
 
 Console.Clear();
 int reavaliar;
-int gravidez;
-int Mellitus;
 
 
 Console.WriteLine("------------Diabetes gestacional------------");
 
 Console.WriteLine("Digite a sua glicose:");
 
-Console.ReadLine();
-
-
-Console.Write("reavaliar entre 24 e 28 semanas com TTOG 75g de glicose");
 reavaliar = Convert.ToInt32(Console.ReadLine());
 
-{
-if( reavaliar <= 92)
+
+if(reavaliar < 92)
 {
     Console.ForegroundColor = ConsoleColor.Blue;
-}
+
+Console.Write("reavaliar entre 24 e 28 semanas com TTOG 75g de glicose");
 }
 
-Console.Write("Compatível com Diabetes Gestacional");
-gravidez = Convert.ToInt32(Console.ReadLine());
-
+if( reavaliar < 126 && reavaliar >= 92)
 {
-if(gravidez >= 92)
+Console.ForegroundColor = ConsoleColor.Red;
+Console.Write("Compatível com Diabetes Gestacional");}
+    
+if(reavaliar >= 126)
 {
-    Console.ForegroundColor = ConsoleColor.Red;
-}
-}
-
-Console.Write("Compatível com Diabetes Gestacional");
-gravidez = Convert.ToInt32(Console.ReadLine());
-{
-if( gravidez <= 126)
-{
-    Console.ForegroundColor = ConsoleColor.Red;
-}
+Console.ForegroundColor = ConsoleColor.Green;
+Console.Write("Diabetes Mellitus na gravidez");
 }
 
-Console.Write("Compatível com Diabetes Gestacional");
-Mellitus = Convert.ToInt32(Console.ReadLine());
-{
-if(Mellitus <= 126)
-{
-    Console.ForegroundColor = ConsoleColor.Green;
-}
-}
+Console.ResetColor();
